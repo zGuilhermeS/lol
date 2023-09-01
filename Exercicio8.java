f84930522cd35038f56b13ec896ed2bc5b44170a
zGuilhermeS <143765545+zGuilhermeS@users.noreply.github.com>
2023-08-31 22:23:22 -0300

    2

--- Exercicio8.java
+++ Exercicio8.java
@@ -1,10 +1,40 @@
 package Exercicio5;
 
-public class Exercicio8 {
+ 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.util.Scanner;
 
-	}
+ 
+
+    public class Exercicio8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite a quantidade total de eleitores do municipío : ");
+        float quantidadeTotalEleitores= sc.nextInt();
+
+        System.out.println("Digite a quantidade de votos brancos do municipío : ");
+        int quantidadeVotosBrancos= sc.nextInt();
+
+        System.out.println("Digite a quantidade de votos nulos : ");
+        float quantidadeVotosNulos= sc.nextInt();
+
+        System.out.println("Digite a quantidade total de votos válidos do municipío : ");
+        float quantidadeTotalVotosValidos= sc.nextInt();
+
+        //Processamento
+        float porcentagemVotosBrancos = quantidadeVotosBrancos/quantidadeTotalEleitores*100;
+
+       
+        //Saída
+        System.out.println("A porcentagem de votos brancos corresponde a : "+porcentagemVotosBrancos + "%");
+
+sc.close();
+    }
 
 }
+
+		 
+		
+	
